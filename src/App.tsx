9adf3a06e346ce7ecd94c26c9148a784969436c9
SaiRohithVennu <vennusairohith@gmail.com>
2025-07-17 18:39:14 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -206,7 +206,7 @@ function App() {
 
         {/* Right Panel - Crowd Analytics */}
         <DraggablePanel
-          title="Apple Vision"
+          title="AI Vision"
           initialPosition={{ x: window.innerWidth - 340, y: 100 }}
           initialSize={{ width: 320, height: 280 }}
           className="z-40"
